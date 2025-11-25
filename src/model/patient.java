be0b7b68055a0e0b2e8e3eeaad6abff3d1ee63f4
Syed-Arham-Ali01 <ssalibplhd@gmail.com>
2025-11-25 21:22:39 +0530

    Update patient.java

--- src/model/patient.java
+++ src/model/patient.java
@@ -1,1 +1,40 @@
+package model;
+
+public class Patient {
+
+    private int patientId;
+    private String name;
+    private int age;
+    private String contactNumber;
+
+    public Patient(int patientId, String name, int age, String contactNumber) {
+        this.patientId = patientId;
+        this.name = name;
+        this.age = age;
+        this.contactNumber = contactNumber;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public String toString() {
+        return "Patient ID: " + patientId +
+                ", Name: " + name +
+                ", Age: " + age +
+                ", Contact: " + contactNumber;
+    }
+}
 
