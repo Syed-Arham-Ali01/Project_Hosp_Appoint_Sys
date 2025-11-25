568f6ed661a326b47f27ae548c6b5c281818638e
Syed-Arham-Ali01 <ssalibplhd@gmail.com>
2025-11-25 21:23:12 +0530

    Update doctor.java

--- src/model/doctor.java
+++ src/model/doctor.java
@@ -1,1 +1,33 @@
+package model;
+
+public class Doctor {
+
+    private int doctorId;
+    private String name;
+    private String specialization;
+
+    public Doctor(int doctorId, String name, String specialization) {
+        this.doctorId = doctorId;
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public String toString() {
+        return "Doctor ID: " + doctorId +
+                ", Name: " + name +
+                ", Specialization: " + specialization;
+    }
+}
 
