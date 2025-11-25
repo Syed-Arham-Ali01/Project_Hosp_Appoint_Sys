89c7f583a4ee1babaa8ff50e46af7859b0855a45
Syed-Arham-Ali01 <ssalibplhd@gmail.com>
2025-11-25 21:23:32 +0530

    Update Appointment.java

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,1 +1,54 @@
+package model;
+
+public class Appointment {
+
+    private int appointmentId;
+    private Patient patient;
+    private Doctor doctor;
+    private String date;
+    private String timeSlot;
+    private String status;
+
+    public Appointment(int appointmentId, Patient patient, Doctor doctor, String date, String timeSlot) {
+        this.appointmentId = appointmentId;
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+        this.timeSlot = timeSlot;
+        this.status = "Booked";
+    }
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTimeSlot() {
+        return timeSlot;
+    }
+
+    public void cancel() {
+        status = "Cancelled";
+    }
+
+    public String toString() {
+        return "Appointment ID: " + appointmentId +
+                ", Patient: " + patient.getName() +
+                ", Doctor: " + doctor.getName() +
+                ", Date: " + date +
+                ", Time: " + timeSlot +
+                ", Status: " + status;
+    }
+}
 
