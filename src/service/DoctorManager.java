8453a4974c01559c22b8e46e1414aaefe61e65b7
Syed-Arham-Ali01 <ssalibplhd@gmail.com>
2025-11-25 21:24:21 +0530

    Update DoctorManager.java

--- src/service/DoctorManager.java
+++ src/service/DoctorManager.java
@@ -1,1 +1,35 @@
+package service;
+
+import model.Doctor;
+import java.util.ArrayList;
+
+public class DoctorManager {
+
+    private ArrayList<Doctor> doctors = new ArrayList<>();
+
+    public void addDoctor(Doctor d) {
+        doctors.add(d);
+        System.out.println("Doctor added successfully!");
+    }
+
+    public void listDoctors() {
+        if (doctors.isEmpty()) {
+            System.out.println("No doctors available.");
+            return;
+        }
+        for (Doctor d : doctors) {
+            System.out.println(d);
+        }
+    }
+
+    public Doctor getDoctorById(int id) {
+        for (Doctor d : doctors) {
+            if (d.getDoctorId() == id) {
+                return d;
+            }
+        }
+        System.out.println("Doctor not found!");
+        return null;
+    }
+}
 
